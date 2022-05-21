ffd62376b3cb3c760dd8089af362058a6fd41d96
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-21 19:33:09 +0600

    manage doctor

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ReturnDocument } = require("mongodb");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const res = require("express/lib/response");
 require("dotenv").config();
 
 app.use(cors());
@@ -40,6 +41,21 @@ async function run() {
       .collection("services");
     const bookingCollection = client.db("doctors_portal").collection("booking");
     const userCollection = client.db("doctors_portal").collection("users");
+    const doctorCollection = client.db("doctors_portal").collection("doctors");
+
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "Forbidden" });
+      }
+    };
 
     app.get("/services", async (req, res) => {
       const query = {};
@@ -67,10 +83,16 @@ async function run() {
       );
       res.send({ result, token });
     });
-    app.put("/users/admin/:email", verifyJWT, async (req, res) => {
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+    app.put("/users/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
-
       const updateDoc = {
         $set: { role: "admin" },
       };
@@ -148,6 +170,16 @@ async function run() {
         const result = await bookingCollection.insertOne(booking);
         res.send({ success: true, result });
       }
+    });
+    app.get("/doctor", verifyJWT, verifyAdmin, async (req, res) => {
+      const doctors = await doctorCollection.find().toArray();
+
+      res.send(doctors);
+    });
+    app.post("/doctor", verifyJWT, verifyAdmin, async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
     });
   } finally {
   }
